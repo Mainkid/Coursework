1cf0985288bea6c096af98ba134dac5ad52d9511
Mainkidd0 <aosipov@lab-code.com>
2021-11-05 18:07:59 +0500

    123321

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import vk
 
 
 def print_hi(name):
@@ -11,6 +8,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    session = vk.Session(access_token="a581352b328e9c7f6a4edf77193b962e32ad8db6ffc7c95fb55a801318a290f192efbc69801656e83b4c2")  # Начинаем сессию
+    vk_api = vk.API(session)
+    vk_api.wall.getComments(owner_id=228985481, v=5.126, post_id=1009,
+                                 thread_items_count=10)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
